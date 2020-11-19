2181b841c1d45f115ad703872a54880349499211
RicardoReyes44 <rickyboy1002@gmail.com>
2020-11-19 16:49:54 -0600

    Se agregaron las mediciones de tiempo en todos los metodos de la clase estatica interna 'Burbuja' y dentro de la clase 'MetodosOrdenamiento'

--- src/PruebaMetodosOrdenamiento.java
+++ src/PruebaMetodosOrdenamiento.java
@@ -8,6 +8,7 @@ class MetodosOrdenamiento{
        
        public static void ordenacionBurbuja1(int []numeros) {
 			
+    	   tInicio = System.nanoTime();
 			for(int i=1; i<=numeros.length-1; i++) {
 				
 				for(int j=0; j<=numeros.length-i-1; j++) {
@@ -18,11 +19,13 @@ class MetodosOrdenamiento{
 						numeros[j+1] = aux;
 					}
 				}
-			}	
+			}
+			tFin = System.nanoTime();
+			System.out.println("Tiempo de ejecucion en ordenamiento por burbuja: " + (tFin-tInicio));
 		}
 		
         public static void ordenacionBurbuja2(int []numeros) {
-        	
+        	tInicio = System.nanoTime();
             for(int i=1; i<numeros.length; i++) {
 				
 				for(int j=0; j<numeros.length-i; j++) {
@@ -33,11 +36,13 @@ class MetodosOrdenamiento{
 						numeros[j+1] = aux;
 					}
 				}
-			}	
+			}
+            tFin = System.nanoTime();
+			System.out.println("Tiempo de ejecucion en ordenamiento por burbuja: " + (tFin-tInicio));
 		}
         
         public static void ordenacionBurbuja3(int []numeros) {
-        	
+           tInicio = System.nanoTime();
            int i=1;
      	   do {
      		   
@@ -52,7 +57,8 @@ class MetodosOrdenamiento{
      		   
      		   i=1+i;
      	   }while(i<numeros.length);
-        	
+     	    tFin = System.nanoTime();
+			System.out.println("Tiempo de ejecucion en ordenamiento por burbuja: " + (tFin-tInicio));
         }
         
 	}// class Burbuja
